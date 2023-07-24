a5910331cd646cf2d8f12d6cc4c60e8d84446163
MdRahil <md.rahil@chaincodeconsulting.com>
2023-07-24 16:46:18 +0530

    login test suit commit

--- Test Suites/NFTrace/login.ts
+++ Test Suites/NFTrace/login.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Login-Logout</name>
+   <name>login</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,19 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>fa56ce5d-ac91-4309-80ab-39c0e887d787</testSuiteGuid>
+   <testSuiteGuid>c597ce2a-3e73-4875-a670-9294b2a022de</testSuiteGuid>
    <testCaseLink>
-      <guid>c9facabe-54dd-4f02-a32a-90b8d7082d00</guid>
+      <guid>a1ae0bbf-7c06-4214-8c89-f84c0e2c3781</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/NFTrace/Login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0d172bd8-b6d7-48d4-bc4d-55c5160410a4</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/NFTrace/Form Builder</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
